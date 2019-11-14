84b5bf44c3a0a87231bcaa09d5621b94d6aa9d43
ANIL DOGAN <20170808071@ogr.akdeniz.edu.tr>
2019-11-14 22:32:07 +0300

    Project 1 added

---
+++ Project-1/sailcovar2.jl
@@ -0,0 +1,27 @@
+using JuMP, Clp, Printf
+
+d = [40 60 75 25]                   # monthly demand for boats
+
+m = Model(with_optimizer(Clp.Optimizer))
+
+@variable(m, 0 <= x[1:4] <= 40)       # boats produced with regular labor
+@variable(m, y[1:4] >= 0)             # boats produced with overtime labor
+@variable(m, h[1:5] >= 0)             # boats held in inventory
+@variable(m, inc[1:4] >= 0)
+@variable(m, dec[1:4] >= 0)  
+
+@constraint(m, h[1] == 10)
+@constraint(m, h[5] >= 10)
+@constraint(m, flow[i in 1:4], h[i]+x[i]+y[i] == d[i]+h[i+1])  
+@constraint(m, x[1]+y[1]-50 == inc[1]+dec[1])
+@constraint(m, flow1[i in 2:4], x[i]+y[i]-(x[i-1]+y[i-1]) == inc[i]+dec[i])     # conservation of boats
+
+@objective(m, Min, 400*sum(x) + 450*sum(y) + 20*sum(h) + 400*sum(inc) + 500*sum(dec))         # minimize costs
+
+optimize!(m)
+
+@printf("Boats to build regular labor: %d %d %d %d\n", value(x[1]), value(x[2]), value(x[3]), value(x[4]))
+@printf("Boats to build extra labor: %d %d %d %d\n", value(y[1]), value(y[2]), value(y[3]), value(y[4]))
+@printf("Inventories: %d %d %d %d %d\n ", value(h[1]), value(h[2]), value(h[3]), value(h[4]), value(h[5]))
+
+@printf("Objective cost: %f\n", objective_value(m))
